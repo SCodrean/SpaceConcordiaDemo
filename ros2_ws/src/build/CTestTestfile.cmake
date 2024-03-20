d6074f1cc62d6e8ff1753fa5930be0b89a83439b
SCodrean <stefanvcodrean@gmail.com>
2024-03-20 11:09:59 -0400

    Created py_subscriber with packages

---
+++ ros2_ws/src/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/stefan-linux/repos/SpaceConcordiaDemo/ros2_ws/src/cpp_pub
+# Build directory: /home/stefan-linux/repos/SpaceConcordiaDemo/ros2_ws/src/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
